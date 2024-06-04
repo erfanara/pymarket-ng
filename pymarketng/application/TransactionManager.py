249f88e6f905a37216135ce12c1e1197d0e26282
erfanara <m.erfann.ara@gmail.com>
2024-06-04 20:18:01 +0330

    add: stat functions

--- pymarketng/application/TransactionManager.py
+++ pymarketng/application/TransactionManager.py
@@ -10,8 +10,20 @@ class TransactionManager:
     def add_transaction(self, *transactions: Transaction):
         self.trans.extend(transactions)
 
-    def add_transactions(self, bids: list[Transaction]):
-        self.trans.extend(bids)
+    def add_transactions(self, transactions: list[Transaction]):
+        self.trans.extend(transactions)
 
     def get_df(self):
         return pd.json_normalize([t.as_dict() for t in self.trans])
+
+    def get_players_total_profit(self):
+        return sum([t.quantity * ((t.buyyer_bid.price - t.buy_price)+(t.sell_price - t.seller_bid.price)) for t in self.trans])
+
+    def get_players_total_quantity(self):
+        return sum([t.quantity for t in self.trans])
+
+    def get_auctineer_profit(self):
+        return sum([t.quantity * (t.buy_price - t.sell_price) for t in self.trans])
+
+    def stats(self):
+        pass
