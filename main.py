621058ece9d969fe824bed0c779508ac6fff5987
erfanara <m.erfann.ara@gmail.com>
2024-07-20 21:40:58 +0330

    now market is able to run the simulation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pymarketng.application.Market import Market, bid_selector_1h, mechanism_selctor_avg
 from pymarketng.application.Mechanism import *
 from pymarketng.application.BidManager import BidManager
 from pymarketng.application.Plot import plot_demand_curves, plot_trades_as_graph
@@ -8,8 +9,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
+import inspect
+from typing import Callable, List, Type
+
 # 1 Round of auction dataframe
-n = 4
+n = 10
 data = {
     "user": np.random.randint(1, 21, n),
     "type": np.random.randint(1, 4, n),
@@ -33,33 +37,37 @@ df = pd.DataFrame(data)
 bm = BidManager()
 
 # create bids from df
-# for index, row in df.iterrows():
-#     bm.add_bid(Bid(row["price"],row["user"],row["unit"],row["buying"],row["time"]))
+for index, row in df.iterrows():
+    bm.add_bid(Bid(row["price"],row["user"],row["unit"],row["buying"],row["time"]))
 
-bm.add_bid(Bid(178.0, 20, 84))
-bm.add_bid(Bid(128.0, 20, 54, False))
-bm.add_bid(Bid(132.0, 7, 13))
-bm.add_bid(Bid(56.0, 7, 51))
+# bm.add_bid(Bid(178.0, 20, 84))
+# bm.add_bid(Bid(128.0, 20, 54, False))
+# bm.add_bid(Bid(132.0, 7, 13))
+# bm.add_bid(Bid(56.0, 7, 51))
 
-print("Bids users")
-for b in bm.buyyers:
-    print("b", b.user)
-for s in bm.sellers:
-    print("s", s.user)
+print(bm.get_df())
+# print("Bids users")
+# for b in bm.buyyers:
+#     print("b", b.user)
+# for s in bm.sellers:
+#     print("s", s.user)
 
-print("after add_bids bm")
-for k,v in bm.um.users.items():
-    print(v)
+# print("after add_bids bm")
+# for k,v in bm.um.users.items():
+#     print(v)
 
-# print(bm.get_df())
-bm_new,tm=bm.run(Average_Mechanism_Multi)
+# # print(bm.get_df())
+# bm_new,tm=bm.run(Average_Mechanism_Multi)
 
-print("after mechanism bm_new")
-for k,v in bm_new.um.users.items():
-    print(v)
-# print(bm.um.get_df())
-# print(bm_new.um.get_df())
+# print("after mechanism bm_new")
+# for k,v in bm_new.um.users.items():
+#     print(v)
+# # print(bm.um.get_df())
+# # print(bm_new.um.get_df())
 
-# tm.get_df()
-# bm_new.get_df()
-# bm_new.um.get_df()
+# # tm.get_df()
+# # bm_new.get_df()
+# # bm_new.um.get_df()
+
+m=Market(mechanism_selctor_avg, bid_selector_1h, bm.get_df())
+m.run()
--- pymarketng/application/Market.py
+++ pymarketng/application/Market.py
@@ -1,49 +1,104 @@
 import inspect
-from typing import Callable, List, Type
+from typing import Callable, Generator, List, Type
 
 from pymarketng.application.BidManager import BidManager
 from pymarketng.application.Mechanism import Average_Mechanism_Multi, Mechanism
 from pymarketng.application.TransactionManager import TransactionManager
+from pymarketng.domain.Bid import Bid
+
+import pandas as pd
+import numpy as np
+
+
+def check_typing(a: Callable, b: Callable):
+    a_sign = inspect.signature(a)
+    b_sign = inspect.signature(b)
+    return a_sign == b_sign
 
 
 class Market:
-    def __init__(self, mechanism_selector: Callable) -> None:
-        self.__check_signature_of_mechanism_selector(mechanism_selector)
+    def __init__(
+        self,
+        mechanism_selector: Callable,
+        bids_selector: Callable,
+        all_bids,
+        *args,
+        **kwargs,
+    ) -> None:
+        check_typing(mechanism_selector, Market.mechanism_selctor_template)
+        check_typing(bids_selector, Market.bids_selctor_template)
+        self.args = args
+        self.kwargs = kwargs
+        self.all_bids = all_bids
         self.mechanism_selector = mechanism_selector
+        self.bids_selector = bids_selector
         self.bm_list: List[BidManager] = []
         self.tm_list: List[TransactionManager] = []
-        self.current_bm: BidManager = BidManager()
-        self.bm_list.append(self.current_bm)
+        # Create a BidManager for the first round
+        self.bm_list.append(BidManager())
 
-    def __check_signature_of_mechanism_selector(self, func):
-        signature = inspect.signature(func)
-        if len(signature.parameters) != 2:
-            raise ValueError("Function must have exactly 2 parameters")
-        if len(signature.parameters) != 2:
-            raise ValueError("Function must have exactly 2 parameters")
-        if list(signature.parameters.keys()) != ["bm_list", "tm_list"]:
-            raise ValueError(
-                "Function parameters must be named 'bm_list' and 'tm_list'"
-            )
-        if signature.parameters["bm_list"].annotation != List[BidManager]:
-            raise ValueError("bm_list parameter must be a list of BidManager")
-        if signature.parameters["tm_list"].annotation != List[TransactionManager]:
-            raise ValueError("tm_list parameter must be a list of TransactionManager")
-        if signature.return_annotation != Type[Mechanism]:
-            raise ValueError("Function must return a subclass of Mechanism")
+    # generator: no return needed
+    @staticmethod
+    def mechanism_selctor_template(
+        bm_list: List[BidManager], tm_list: List[TransactionManager], *args, **kwargs
+    ) -> Generator[type[Mechanism], None, None]:
+        yield Mechanism
 
-    def add(self):
-        pass
+    @staticmethod
+    def bids_selctor_template(all_bids, *args, **kwargs) -> Generator[List[Bid], None, None]:
+        yield []
 
-    def run(self):
-        while True:
-            next_mechanism = self.mechanism_selector(self.bm_list, self.tm_list)
-            bm_new, tm = self.current_bm.run(next_mechanism)
+    def run(self):        
+        current_bm = self.bm_list[-1]
+        bids_iterator = self.bids_selector(self.all_bids, *self.args, **self.kwargs)
+        mechanism_iterator = self.mechanism_selector(self.bm_list, self.tm_list, *self.args, **self.kwargs)
+        for bids in bids_iterator:
+            # print(bids)
+            # for each round, first add the bids, and then run the mechanism
+            current_bm.add_bid(*bids)
+            bm_new, tm = current_bm.run(next(mechanism_iterator))
+            print(tm.get_df())
+
             self.bm_list.append(bm_new)
             self.tm_list.append(tm)
-            self.current_bm = bm_new
+            current_bm = bm_new
+
 
 def mechanism_selctor_avg(
-    bm_list: List[BidManager], tm_list: List[TransactionManager]
-) -> Type[Mechanism]:
-    return Average_Mechanism_Multi
+    bm_list: List[BidManager], tm_list: List[TransactionManager], *args, **kwargs
+) -> Generator[Type[Mechanism], None, None]:
+    while True:
+        yield Average_Mechanism_Multi
+
+
+# TODO: this type of function is not suitable for our work, i guess iterator is what we need
+# TODO: it's not obvious that all_bids is a List[Bid] or Dataframe
+def bid_selector_1h(all_bids, *args, **kwargs) -> Generator[List[Bid], None, None]:
+    df = pd.DataFrame(all_bids)
+    df["time"] = pd.to_datetime(df["time"])
+
+    # loop through each unique date
+    for date in np.sort(df["time"].dt.date.unique()):
+        # filter the dataframe for the current date
+        date_df = df[df["time"].dt.date == date]
+        # loop through each hour
+        for hour in np.sort(date_df["time"].dt.hour.unique()):
+            # filter the dataframe for the current hour
+            hourly_df = date_df[(date_df["time"].dt.hour == hour)]
+
+            # TODO: why converting df to Bids? 
+            # (There is a bug in entire project that we convert
+            #  data to Bids and then Dataframes consequently and
+            #  there is no such a law to obeoy in entire project)
+            bids = []
+            for i, row in hourly_df.iterrows():
+                bids.append(
+                    Bid(
+                        user_id=row["user"],
+                        price=row["price"],
+                        quantity=row["quantity"],
+                        buying=not row["buying"],
+                        time=row["time"],
+                    )
+                )
+            yield bids
