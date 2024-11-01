b384feb5802c0f8d12e9dd9c4b4bf8c5dd2d0c15
erfan ara <m.erfann.ara@gmail.com>
2024-11-01 17:12:11 +0330

    [UPDATE] breaking change: mechanism selector now yileds a tuple of mechanisms

--- pymarketng/application/Market.py
+++ pymarketng/application/Market.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Callable, Generator, List, Type
+from typing import Callable, Generator, List, Type, Tuple
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -31,14 +31,23 @@ class MechanismSelector(ABC):
     To select a mechanism for each round, we have a select() function.
     """
 
+    def __init__(
+        self,
+        bm_list: List[BidsManager],
+        tm_list: List[TransactionManager],
+        *args,
+        **kwargs,
+    ):
+        self.bm_list = bm_list
+        self.tm_list = tm_list
+        self.args = args
+        self.kwargs = kwargs
+
     # features
     parallel_run = False
 
-    @staticmethod
     @abstractmethod
-    def select(
-        bm_list: List[BidsManager], tm_list: List[TransactionManager], *args, **kwargs
-    ) -> Generator[Type[Mechanism], None, None]:
+    def select(self) -> Generator[Tuple[Type[Mechanism], ...], None, None]:
         """
         This function is a generator that can be used as iterator,
         Functionality: It should yield a Mechanism for each iteration.
@@ -46,7 +55,7 @@ class MechanismSelector(ABC):
         This function is useful for users to implement their logic of
         Mechanism selction over multiple rounds whithin the market.
         """
-        yield Mechanism
+        yield (Mechanism,)
 
 
 class Market:
@@ -82,41 +91,43 @@ class Market:
         yield pd.DataFrame()
 
     def run_serial(self, *args):
-        current_bm = self.bm_list[-1]
         bids_iterator = self.bids_selector(self.all_bids, *self.args, **self.kwargs)
-        mechanism_iterator = self.mechanism_selector.select(
+
+        mechanism_iterator = self.mechanism_selector(
             self.bm_list, self.tm_list, *self.args, **self.kwargs
-        )
+        ).select()
         for bids_df in bids_iterator:
+            current_bm = self.bm_list[-1]
+
             # for each BidsManager, first add the bids, and then run the mechanism
             current_bm.add_bids(bids_df)
-            bm_new, tm = current_bm.run(next(mechanism_iterator), *args)
+            results = current_bm.run(next(mechanism_iterator), *args)
 
-            self.bm_list.append(bm_new)
-            self.tm_list.append(tm)
-            current_bm = bm_new
+            self.bm_list.extend([r[0] for r in results])
+            self.tm_list.extend([r[1] for r in results])
 
     def run_parallel(self, *args):
-        current_bm = self.bm_list[-1]
         bids_iterator = self.bids_selector(self.all_bids, *self.args, **self.kwargs)
-        mechanism_iterator = self.mechanism_selector.select(
-            self.bm_list, self.tm_list, *self.args, **self.kwargs
-        )
-        mechanisms_list = list(mechanism_iterator)
 
-        for bids_df in bids_iterator:
-            current_bm.add_bids(bids_df)
-            current_bm = BidsManager()
-            self.bm_list.append(current_bm)
+        mechanism_iterator = self.mechanism_selector(
+            self.bm_list, self.tm_list, *self.args, **self.kwargs
+        ).select()
 
         with ThreadPoolExecutor(max_workers=8) as executor:
-            futures = {
-                executor.submit(bm.run, mc, *args): (bm, mc)
-                for bm, mc in zip(self.bm_list, mechanisms_list)
-            }
+            futures = {}
+            for bids_df in bids_iterator:
+                current_bm = self.bm_list[-1]
+
+                # for each BidsManager, first add the bids, and then run the mechanism
+                current_bm.add_bids(bids_df)
+                mc = next(mechanism_iterator)
+                futures[executor.submit(current_bm.run, mc, *args)] = (current_bm, mc)
+
+                # add a new BidManager for the next round
+                self.bm_list.append(BidsManager())
             for future in as_completed(futures):
                 result = future.result()
-                self.tm_list.append(result)
+                self.tm_list.extend([r[1] for r in result])
 
     def run(self, *args):
         if self.mechanism_selector.parallel_run:
@@ -158,31 +169,11 @@ class Market:
         # is vcg affordable
 
 
-class MechanismSelectorAverage(MechanismSelector):
-    @staticmethod
-    def select(
-        bm_list: List[BidsManager], tm_list: List[TransactionManager], *args, **kwargs
-    ) -> Generator[Type[Mechanism], None, None]:
-        while True:
-            yield Average_Mechanism_Multi
-
-
 def bid_selector_1h(
     all_bids: pd.DataFrame, *args, **kwargs
 ) -> Generator[pd.DataFrame, None, None]:
-    df = all_bids
-
-    # loop through each unique date
-    for date in np.sort(df["time"].dt.date.unique()):
-        # filter the dataframe for the current date
-        date_times = df.loc[df["time"].dt.date == date, "time"]
-        # loop through each hour
-        for hour in np.sort(date_times.dt.hour.unique()):
-            # filter the dataframe for the current hour
-            hourly_df = df.loc[
-                (df["time"].dt.date == date) & (df["time"].dt.hour == hour)
-            ]
-
-            yield hourly_df.copy()
-            # yield a empty dataframe TODO: needs more explanation on why
-            yield pd.DataFrame(BidsManager.df_template)
+    grouped = all_bids.groupby([all_bids['time'].dt.date, all_bids['time'].dt.hour])
+    for (date, hour), group in grouped:
+        print(date, hour)
+        yield group.copy()
+        yield pd.DataFrame(BidsManager.df_template)
